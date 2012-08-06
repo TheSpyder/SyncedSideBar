b124b0faa420c8a7ef3fd551543b5b4469e06206
Andrew Herron <andrew.herron@ephox.com>
2012-08-07 08:06:45 +1000

    Fixed #10, when the setting was not specified it defaulted to false

--- SyncedSideBar.py
+++ SyncedSideBar.py
@@ -5,5 +5,5 @@ class SideBarListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
         active = view.window()
-        if active and view.settings().get('reveal-on-activate'):
+        if active and view.settings().get('reveal-on-activate') != False:
             active.run_command('reveal_in_side_bar')
