3609cc3a8f08ccb52d753ac98914772e4aa7350a
Todd Wolfson <todd@twolfson.com>
2013-01-19 04:21:58 -0800

    Added enable/disable commands

--- SyncedSideBar.py
+++ SyncedSideBar.py
@@ -1,3 +1,4 @@
+import sublime
 import sublime_plugin
 
 
@@ -7,3 +8,9 @@ class SideBarListener(sublime_plugin.EventListener):
         active = view.window()
         if active and view.settings().get('reveal-on-activate') != False:
             active.run_command('reveal_in_side_bar')
+
+
+class SideBarEnableSync(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        sublime.error_message("Hello World!")
