ed7337a3df59202248f8ec0264c3cf41f95114d7
Andrew Herron <andrew.herron@ephox.com>
2013-07-24 11:03:33 +1000

    renamed 'active' to 'window', added view tracking to prevent activation after alt+tab. Fixes #17.

--- SyncedSideBar.py
+++ SyncedSideBar.py
@@ -5,7 +5,8 @@ import sublime_plugin
 DEFAULT_VISIBILITY = True
 
 sidebar_visible = DEFAULT_VISIBILITY
-lastActive = None
+lastWindow = None
+lastView = None
 
 # Keep track of active windows so we rememeber sidebar_visible for each one
 windows = {}
@@ -14,33 +15,39 @@ windows = {}
 class SideBarListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        global sidebar_visible, lastActive
-        active = view.window()
+        global lastView
+        if lastView is not None and lastView.id() == view.id():
+            # this view has already been processed, likely an alt-tab focus event
+            return
+        lastView = view
 
-        if not active:
+        global sidebar_visible, lastWindow
+        activeWindow = view.window()
+
+        if not activeWindow:
             return
 
-        if not active.id() in windows:
+        if not activeWindow.id() in windows:
             # first activation in this window, use default
-            windows[active.id()] = DEFAULT_VISIBILITY
+            windows[activeWindow.id()] = DEFAULT_VISIBILITY
 
-        if lastActive is None:
+        if lastWindow is None:
             # plugin just loaded
-            lastActive = active
-        elif lastActive != active:
+            lastWindow = activeWindow
+        elif lastWindow.id() != activeWindow.id():
             # store the old window state
-            windows[lastActive.id()] = sidebar_visible
+            windows[lastWindow.id()] = sidebar_visible
             # load the new window state
-            sidebar_visible = windows[active.id()]
-            lastActive = active
+            sidebar_visible = windows[activeWindow.id()]
+            lastWindow = activeWindow
 
         if sidebar_visible and view.settings().get('reveal-on-activate') != False:
-            active.run_command('reveal_in_side_bar')
+            activeWindow.run_command('reveal_in_side_bar')
 
     # Sublime text v3 window command listener, safe to include unconditionally as it's simply ignored by v2.
     def on_window_command(self, window, command_name, args):
-        global sidebar_visible
         if command_name == "toggle_side_bar":
+            global sidebar_visible
             sidebar_visible = not sidebar_visible
 
 
