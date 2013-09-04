5bb0e98eec04c24fef25e043872f06a8d4677cad
Andrew Herron <thespyder@programmer.net>
2013-09-04 13:16:00 +1000

    Massive refactor. Extracted state management and preference reading from EventListener into separate functions. Combined ApplicationCommand listeners, using an 'enable' argument instead of inheritance.

--- SyncedSideBar.py
+++ SyncedSideBar.py
@@ -3,14 +3,16 @@ import sublime_plugin
 
 # assume sidebar is visible by default on every window (there's no way to check, unfortunately)
 DEFAULT_VISIBILITY = True
+sidebarVisible = DEFAULT_VISIBILITY
 
-pref_reveal = DEFAULT_VISIBILITY
+# preference from plugin settings file
+pluginPref = DEFAULT_VISIBILITY
 
-sidebar_visible = DEFAULT_VISIBILITY
-lastWindow = None
+# flag for alt-tab focus check
 lastView = None
 
-# Keep track of active windows so we remember sidebar_visible for each one
+# Keep track of active windows so we remember sidebarVisible for each one
+lastWindow = None
 windows = {}
 
 def plugin_loaded():
@@ -19,8 +21,8 @@ def plugin_loaded():
     def read_pref():
         vis = s.get('reveal-on-activate')
         if vis is not None:
-            global pref_reveal
-            pref_reveal = vis
+            global pluginPref
+            pluginPref = vis
 
     # read initial setting
     read_pref()
@@ -31,78 +33,67 @@ def plugin_loaded():
 if (int(sublime.version()) < 3000):
     plugin_loaded()
 
-def should_be_visible(view):
-    viewSetting = view.settings().get('reveal-on-activate')
-    return viewSetting if viewSetting is not None else pref_reveal
+
+def manage_state(view):
+    activeWindow = view.window()
+
+    if not activeWindow.id() in windows:
+        # first activation in this window, use default
+        windows[activeWindow.id()] = DEFAULT_VISIBILITY
+
+    global sidebarVisible, lastWindow
+    if lastWindow is None:
+        # plugin just loaded
+        lastWindow = activeWindow
+    elif lastWindow.id() != activeWindow.id():
+        # store the old window state
+        windows[lastWindow.id()] = sidebarVisible
+        # load the new window state
+        sidebarVisible = windows[activeWindow.id()]
+        lastWindow = activeWindow
+
+
+def show_view(view):
+    userPref = view.settings().get('reveal-on-activate')
+    reveal = userPref if userPref is not None else pluginPref
+
+    if sidebarVisible and reveal != False:
+        def reveal():
+            view.window().run_command('reveal_in_side_bar')
+
+        # When using quick switch project, the view activates before the sidebar is ready.
+        # This tiny delay is imperceptible but works around the issue.
+        sublime.set_timeout(reveal, 100);
 
 
 class SideBarListener(sublime_plugin.EventListener):
     def on_activated(self, view):
-        activeWindow = view.window()
-
-        # don't even consider updating state if we don't have an activeWindow.
-        # "goto anything" doesn't set activeWindow until the file is selected.
-        # also, reveal in side bar is a window command only.
-        if not activeWindow:
+        # don't even consider updating state if we don't have a window.
+        # reveal in side bar is a window command only.
+        # "goto anything" activates views but doesn't set a window until the file is selected.
+        if not view.window():
             return
 
         global lastView
         if lastView is not None and lastView.id() == view.id():
             # this view has already been processed, likely an alt-tab focus event
             return
-
         lastView = view
 
-        if not activeWindow.id() in windows:
-            # first activation in this window, use default
-            windows[activeWindow.id()] = DEFAULT_VISIBILITY
+        manage_state(view)
+        show_view(view)
 
-        global sidebar_visible, lastWindow
-        if lastWindow is None:
-            # plugin just loaded
-            lastWindow = activeWindow
-        elif lastWindow.id() != activeWindow.id():
-            # store the old window state
-            windows[lastWindow.id()] = sidebar_visible
-            # load the new window state
-            sidebar_visible = windows[activeWindow.id()]
-            lastWindow = activeWindow
-
-        if sidebar_visible and should_be_visible(view) != False:
-            def reveal():
-                activeWindow.run_command('reveal_in_side_bar')
-
-            # When using quick switch project, the view activates before the sidebar is ready.
-            # This tiny delay is imperceptible but works around the issue.
-            sublime.set_timeout(reveal, 100);
 
     # Sublime text v3 window command listener, safe to include unconditionally as it's simply ignored by v2.
     def on_window_command(self, window, command_name, args):
         if command_name == "toggle_side_bar":
-            global sidebar_visible
-            sidebar_visible = not sidebar_visible
+            global sidebarVisible
+            sidebarVisible = not sidebarVisible
 
 
 class SideBarUpdateSync(sublime_plugin.ApplicationCommand):
-    def run(self):
-        pass
-
-    def updateSync(self, value):
-        # Load in user settings
+    # Update user preferences with the new value
+    def run(self, enable):
         settings = sublime.load_settings("Preferences.sublime-settings")
-
-        # Update the setting
-        settings.set("reveal-on-activate", value)
-
-        # Save our changes
+        settings.set("reveal-on-activate", enable)
         sublime.save_settings("Preferences.sublime-settings")
-
-
-class SideBarEnableSync(SideBarUpdateSync):
-    def run(self):
-        self.updateSync(True)
-
-
-class SideBarDisableSync(SideBarUpdateSync):
-    def run(self):
-        self.updateSync(False)
